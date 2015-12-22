fdb98c07fec9136f7ced8b5703f1b0ff2bc487cd
Gponder <qin_job@163.com>
2015-12-22 14:40:24 +0800

    12/11 17:22 tijiao

--- app/src/main/java/com/example/administrator/test/FirstActivity.java
+++ app/src/main/java/com/example/administrator/test/FirstActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.example.administrator.test.netutil.HttpClientAsyncTask;
@@ -24,11 +25,31 @@ public class FirstActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+//      new MyAsyncTask().execute("a","b","c");
+
         for (int i=0;i<500;i++){
             String task = "task@"+i;
-            new MyAsyncTask().execute("a","b","c");
-            new HttpClientAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,task);
+            new TestAsyncTask(task).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,0);
 
+        }
+    }
+    class TestAsyncTask extends AsyncTask{
+        private Object threadPoolTaskData;
+
+        public TestAsyncTask(String s) {
+            this.threadPoolTaskData = s;
+        }
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            Log.i("testasynctask","start..."+threadPoolTaskData+"threadid:"+Thread.currentThread().getId()+"threadname:"+Thread.currentThread().getName());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            threadPoolTaskData=null;
+            return null;
         }
     }
 }
